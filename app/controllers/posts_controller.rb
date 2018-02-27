b086d96bc537219646493d96016cc55b6b3bce29
Jandrei Pitti <jp.pitti@yahoo.com>
2018-02-27 15:54:05 -0700

    Update to Functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-      @posts = Post.search(params[:search])
+      @posts = Post.search(params[:search]).order(:created_at)
   end
 
   # GET /posts/1
