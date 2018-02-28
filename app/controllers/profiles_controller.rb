9671a49d0acad80b756e6c987e5f60ff066e5154
Jandrei Pitti <jp.pitti@yahoo.com>
2018-02-27 17:10:59 -0700

    Added some eye candy

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @current_user = @user
+    @search_user = @user
     @posts = @user.posts
     @profile_picture = @user.profile_picture
   end
